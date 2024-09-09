294fc16e65ce1f140f6cf733d2ad3b8d4ac69ffb
DevPeak Innovations <devpeakinnovations@gmail.com>
2024-09-09 02:50:16 +0000

    chatapp module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatappModule } from './chatapp/chatapp.module';
 
 @Module({
-  imports: [],
+  imports: [ChatappModule],
   controllers: [AppController],
   providers: [AppService],
 })
